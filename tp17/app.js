d0ff6ef737fc2f9932abe01e3e087331bbc36d87
ositoCruz <mjj_pc@hotmail.com>
2023-11-15 19:54:32 -0300

    primer commit

---
+++ tp17/app.js
@@ -0,0 +1,27 @@
+const exp = require('constants');
+const express = require('express');
+const path = require("path");
+
+
+const app = express();
+
+app.listen(3030, () => {
+
+console.log("Servidor funcionando")
+
+})
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+  });
+
+
+  app.get("/register", (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"));
+  });
+
+  app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+  });
