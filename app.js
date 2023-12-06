2feda2341fe7f7335c81fb6b7346f36c5e98cdb2
lorenang <lorena.natali18@gmail.com>
2023-12-06 00:08:42 -0300

    SPRINT 3

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
+const mainRoutes = require('./routes/mainRoutes');
 
+app.use(express.static('./public'));
+// Configurar EJS como motor de plantillas
+app.set('view engine', 'ejs');
 
+app.use('/', mainRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`click para dirigirte al sitio: http://localhost:${PORT}/`);
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
-});
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/views/index.html');
+// });
 
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/login.html');
+// });
 
-app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + '/views/carritoDeCompras.html')});
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-app.get('/details', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
-});
-
-//pude hacer un commit y les voy a comprar un helado a los profes que me ayudaron
+// app.get('/carrito', (req,res)=>{
+//     res.sendFile(__dirname + '/views/carritoDeCompras.html')});
+// app.get('/register', (req,res)=>{
+//     res.sendFile(__dirname + '/views/register.html');
+// });
+// app.get('/details', (req,res)=>{
+//     res.sendFile(__dirname + '/views/productDetail.html');
+// });
