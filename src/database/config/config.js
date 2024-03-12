d2b149aadc2a2cc50cf209d0de71c6be161dbcdc
Lorena <lorena.natali18@gmail.com>
2024-03-11 23:02:47 -0300

    merg

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,15 @@
 module.exports = {
   "development": {
     "username": "root",
-<<<<<<< HEAD
     "password": "root",
-    "database": "PurumAgroInsumos",
-=======
-    "password": "",
     "database": "Purumnuevo",
->>>>>>> 31f473ddc8a790e375532a723c25be865c1eee3e
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-<<<<<<< HEAD
     "password": "root",
-    "database": "PurumAgroInsumos",
-=======
-    "password": "",
     "database": "Purumnuevo",
->>>>>>> 31f473ddc8a790e375532a723c25be865c1eee3e
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
